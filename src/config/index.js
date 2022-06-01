9316e354b59fb53198e9c377e2b75cb043f7b3ed
bastien-cordier <bastien.cordier78@gmail.com>
2022-06-01 19:50:28 +0200

    Hide secret key

--- src/config/index.js
+++ src/config/index.js
@@ -2,5 +2,4 @@
 // export const API_KEY = process.env.REACT_APP_API_KEY;
 export const API_URL =
   process.env.REACT_APP_API_URL || 'https://api.spoonacular.com';
-export const API_KEY =
-  process.env.REACT_APP_API_KEY || '40e7cd50e290462496c0ba815bc532f9';
+export const API_KEY = process.env.REACT_APP_API_KEY || '';
