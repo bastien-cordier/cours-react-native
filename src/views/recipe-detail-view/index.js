352f4eb4648d3649df1304e1b38745a53c1cc694
bastien-cordier <bastien.cordier78@gmail.com>
2022-06-02 15:46:32 +0200

    modif deuxieme jour

--- src/views/recipe-detail-view/index.js
+++ src/views/recipe-detail-view/index.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Text, View} from 'native-base';
+import __recipies from '../../constants/recipes';
 
-export const RecipeDetailView = () => {
+export const RecipeDetailView = ({navigation, route}) => {
+  const {recipe} = route.params;
+
+  useEffect(() => {
+    console.log('RECIPE', recipe);
+    navigation.setOptions({
+      title: recipe.title,
+    });
+  }, []);
+
   return (
     <View>
-      <Text>RECIPE DETAIL</Text>
+      <Text>RECIPE DETAIL: {recipe.title}</Text>
     </View>
   );
 };
--- src/views/recipes-view/index.js
+++ src/views/recipes-view/index.js
@@ -1,9 +1,10 @@
 import React, {useState, useEffect} from 'react';
-import {View, Text, FlatList} from 'native-base';
+import {StyleSheet} from 'react-native';
+import {View, Pressable, FlatList} from 'native-base';
 import __recipes from '../../constants/recipes';
 import {RecipeCard} from '../../components';
 
-export const RecipesView = () => {
+export const RecipesView = ({navigation}) => {
   const [recipes, setRecipes] = useState([]);
 
   useEffect(() => {
@@ -12,19 +13,38 @@ export const RecipesView = () => {
     setTimeout(() => {
       setRecipes(__recipes);
     }, 1000);
+
+    navigation.setOptions({
+      title: 'Recettes',
+    });
   }, []);
 
   return (
     <View>
-      <Text>Recipies</Text>
       <FlatList
         data={recipes}
         renderItem={({item: recipe}) => {
           console.log('RECIPE', recipe);
-          return <RecipeCard recipe={recipe} />;
+          return (
+            <Pressable
+              onPress={() =>
+                navigation.navigate('recipeDetailView', {
+                  recipe,
+                })
+              }>
+              <RecipeCard recipe={recipe} />
+            </Pressable>
+          );
         }}
         keyExtractor={item => item.id}
+        style={styles.recipiesList}
       />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  recipiesList: {
+    paddingTop: 25,
+  },
+});
